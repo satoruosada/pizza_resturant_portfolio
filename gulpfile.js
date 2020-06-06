a4889de851353843bc8e7b39a1b0ea69fab3ac2b
satoruosada <satoru.5714@gmail.com>
2020-06-06 11:10:32 +0900

    開発スタート

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,78 @@
-//plug-in
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserify = require('browserify');
-var browserSync = require('browser-sync').create();
-var source = require('vinyl-source-stream');
-
-// sassとcssの保存先
-gulp.task('sass', function(){
-    gulp.src('./sass/**/*.scss')
-        .pipe(sass({outputStyle: 'expanded'}))
-        .pipe(gulp.dest('./css/'));
-});
-
-//自動監視
-gulp.task('sass-watch', ['sass'], function(){
-    var watcher = gulp.watch('./sass/**/*.scss', ['sass']);
-    watcher.on('change', function(event) {
-    });
-});
-
-gulp.task('default', ['sass-watch']);
+var browserSync  = require('browser-sync');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var imageminPngquant = require("imagemin-pngquant");
+var imageminMozjpeg = require("imagemin-mozjpeg");
 
 
-// gulpタスクの作成
-gulp.task('build', function(){
-  browserify({
-    entries: ['src/app.js'] // ビルド元のファイルを指定
-  }).bundle()
-    .pipe(source('bundle.js')) // 出力ファイル名を指定
-    .pipe(gulp.dest('css/')); // 出力ディレクトリを指定
-});
 gulp.task('browser-sync', function() {
   browserSync.init({
-    server: {
-      baseDir: "./", // 対象ディレクトリ
-      index: "index.html" //indexファイル名
-    }
+      server: {
+          baseDir: "./",
+          index: "index.html"
+      }
   });
 });
 gulp.task('bs-reload', function () {
   browserSync.reload();
 });
 
-// Gulpを使ったファイルの監視
-gulp.task('default', gulp.series(gulp.parallel('build', 'browser-sync'), function(){
-  gulp.watch('./src/*.js', ['build']);
-  gulp.watch("./*.html", ['bs-reload']);
-  gulp.watch("./css/*.+(js|css)", ['bs-reload']);
-}));
+gulp.task('mincss', function() {
+  return gulp.src("css/*.css")
+    .pipe(cleanCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('css/'));
+});
+
+gulp.task('minjs', function() {
+  return gulp.src("js/*.js")
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('js/'));
+});
+
+var imageminOption = [
+  imageminPngquant({ quality: [0.65, 0.8] }),
+  imageminMozjpeg({ quality: 85 }),
+  imagemin.gifsicle({
+    interlaced: false,
+    optimizationLevel: 1,
+    colors: 256
+  }),
+  imagemin.mozjpeg(),
+  imagemin.optipng(),
+  imagemin.svgo()
+];
+
+gulp.task( 'images', function() {
+  return gulp
+    .src( './img/base/*.{png,jpg,gif,svg}' )
+    .pipe( imagemin( imageminOption ) )
+    .pipe( gulp.dest( './img' ) );
+});
+
+gulp.task('sass', function() {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task( 'default', gulp.series( gulp.parallel( 'browser-sync' )), function(done) {
+  gulp.watch( './*.html', gulp.series( gulp.parallel( 'bs-reload' )));
+  gulp.watch( './css/*.css', gulp.series( gulp.parallel( 'bs-reload' )));
+  gulp.watch( './js/*.js', gulp.series( gulp.parallel( 'bs-reload' )));
+  done();
+});
+
+gulp.task( 'watch', function() {
+  gulp.watch( './sass/**/*.scss', gulp.task( 'sass' ) );
+});
+
+
+
+
+
